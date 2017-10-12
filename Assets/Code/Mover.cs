dccde6c94a24b6ff0099f018fbd00f5ccaaea2ea
Jeke <jerkko.valta@gmail.com>
2017-10-12 10:18:45 +0300

    Implemented WASD-movement for the cube.

--- Assets/Code/Mover.cs
+++ Assets/Code/Mover.cs
@@ -6,6 +6,30 @@ namespace TAMK.VCSExample
 	{
 		void Update()
 		{
+			/* WASD movement start */
+
+			if (Input.GetAxisRaw("Horizontal") > 0)
+			{
+				Vector3 temp = new Vector3(0.1f,0,0);
+				transform.position += temp;
+			}
+			if (Input.GetAxisRaw("Horizontal") < 0)
+			{
+				Vector3 temp = new Vector3(-0.1f,0,0);
+				transform.position += temp;
+			}
+			if (Input.GetAxisRaw("Vertical") > 0)
+			{
+				Vector3 temp = new Vector3(0,0,0.1f);
+				transform.position += temp;
+			}
+			if (Input.GetAxisRaw("Vertical") < 0)
+			{
+				Vector3 temp = new Vector3(0,0,-0.1f);
+				transform.position += temp; 
+			}
+
+			/* WASD movement end */
 		}
 	}
 }
